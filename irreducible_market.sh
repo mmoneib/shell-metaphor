3cbe646e8403e784bcb98eb5415560f6df78518a
Muhammad Moneib <mmoneib@gmx.com>
2023-07-26 02:20:01 +0200

    Added comments to irreducible_market.

--- irreducible_market.sh
+++ irreducible_market.sh
@@ -4,20 +4,21 @@
 #                                                                                           #
 # Employing reductionism has always been used by science to uncover the mysteries of our    #
 # lives using the technique of modelling complex systems in simpler circumstances that      #
-# ignore whatever is considered as noise.. In this  # "thought experiment", I reduce a      #
+# ignore whatever is considered to be noise. In this "thought experiment", I reduce a       #
 # market to its most basic form of 2 participants and 2 products, effectively removing the  #
 # noises of choice, abundance, and scarcity. For this simple model, I choose 2 products,    #
-# a hat and a wig, which are mutually-exclusive in terms of used, yet complement each other #
-# in a certain way. This justifies the cycle of buying and selling between the 2            #
-# participants whose name are chosen based on perceived competitiveness and hair-loss       #
+# a hat and a wig, which can be mutually-exclusive in terms of use, yet complement each     #
+# other in a certain way. This justifies the cycle of buying and selling between the 2      #
+# participants whose names are chosen based on perceived competitiveness and hair-loss      #
 # issues. The opposite transactions are only allowed to be simultaneous here in order to    #
-# prevent monopoly and the subsequent failure of the market.                                #
+# prevent monopoly and the subsequent collapse of the market.                               #
 # The experiment reveals fundamental characteristics about capital markets, sustainability, #
 # and the dynamics of pricing.                                                              #
 #                                                                                           #
 # Conceptualized and developed by: Muhammad Moneib                                          #
 #############################################################################################
 
+# Static parameters
 participant1="Elon"
 wallet1=20
 participant2="Jeff"
@@ -26,7 +27,7 @@ itemA="wig"
 priceA=1
 itemB="hat"
 priceB=1
-
+# Dynamic parameters (references modified during runtime)
 p1Owns="$itemA"
 p1Offer="$priceA"
 p2Owns="$itemB"
@@ -37,7 +38,7 @@ printf "%s has %s in his wallet." "$participant2" "$wallet2"
 while true; do
   printf "%s offers %s for price %s.\n" "$participant1" "$p1Owns" "$p1Offer"
   printf "%s offers %s for price %s.\n" "$participant2" "$p2Owns" "$p2Offer"
-  if [ "$wallet1" -lt "$p2Offer" ] && [ "$wallet1" -lt "$p2Offer" ]; then
+  if [ "$wallet1" -lt "$p2Offer" ] && [ "$wallet1" -lt "$p2Offer" ]; then # Condition to prevent non-simultaneous transaction.
     echo "A mutual transaction fails..."
     if [ "$wallet1" -lt "$p2Offer" ]; then
       printf "%s's wallet has only %s in it. He can't afford to buy %s offered for %s.\n" "$participant1" "$wallet1" "$p2Owns" "$p2Offer"
@@ -46,8 +47,10 @@ while true; do
       printf "%s's wallet has only %s in it. He can't afford to buy %s offered for %s.\n" "$participant2" "$wallet2" "$p1Owns" "$p1Offer"
     fi
     printf "Since one or both participants can't affor a transaction, both items' prices will be reduced by 1.\n"
+    # Prices falling in case of lack of buyers (not because of abundance).
     priceA="$(($priceA-1))"
     priceB="$(($priceB-1))"
+    # Swapping dynamic prices parameters.
     if [ "$p1Owns" == "itemA" ]; then
       p1Offer="$priceA"
       p2Offer="$priceB"
@@ -55,16 +58,18 @@ while true; do
       p1Offer="$priceB"
       p2Offer="$priceA"
     fi
-  else
+  else # Condition of a simultaneous transaction.
     echo "A mutual transaction starts..."
     if [ "$wallet1" -ge "$p2Offer" ]; then
       wallet1="$(($wallet1-$p2Offer))"
       wallet2="$(($wallet2+$p2Offer))"
       printf "%s buys %s from %s for %s and has %s left in his wallet and %s in %s's wallet. The item's price will increase by 1.\n" "$participant1" "$p2Owns" "$participant2" "$p2Offer" "$wallet1" "$wallet2" "$participant2"
       if [ "$p2Owns" == "$itemB" ]; then
+        # Price increasing in search for profit with the next transaction (buy low, sell high).
         priceB="$(($priceB+1))"
         p2Offer="$priceB"
       else
+        # Price increasing in search for profit with the next transaction (buy low, sell high).
         priceA="$(($priceA+1))"
         p2Offer="$priceA"
       fi
@@ -74,13 +79,16 @@ while true; do
       wallet1="$(($wallet1+$p1Offer))"
       printf "%s buys %s from %s for %s and has %s left in his wallet and %s in %s's wallet. The item's price will increase by 1.\n" "$participant2" "$p1Owns" "$participant1" "$p1Offer" "$wallet2" "$wallet1" "$participant1"
       if [ "$p1Owns" == "$itemB" ]; then
+        # Price increasing in search for profit with the next transaction (buy low, sell high).
         priceB="$(($priceB+1))"
         p1Offer="$priceB"
       else
+        # Price increasing in search for profit with the next transaction (buy low, sell high).
         priceA="$(($priceA+1))"
         p1Offer="$priceA"
       fi
     fi
+    # Swapping dynamic prices and ownership parameters.
     tmpOwns="$p1Owns"
     p1Owns="$p2Owns"
     p2Owns="$tmpOwns"
