39c8d5294e5470494c7584821678ef93fd3e0f59
Muhammad Moneib <mmoneib@gmx.com>
2023-08-16 23:37:57 +0200

    Added parameters and help to skill_vs_luck_with_geometric_progression.

--- skill_vs_luck_with_geometric_progression.sh
+++ skill_vs_luck_with_geometric_progression.sh
@@ -1,17 +1,40 @@
 #!/bin/sh
+################################################################################
+# Skill vs. Luck with Geometric Progression                                    #
+#                                                                              #
+# A Monte Carlo simulation of the balance required in life (and a casino) for  #
+# successful geometric progression.                                            #
+#                                                                              #
+# Conceptualized and developed by: Muhammad Moneib                             #
+################################################################################
 
-startingAmount=100
-#bets=(0.1 0.3 0.9 0.81)
-#bets=(0.1)
-bets=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
-#bets=(0.01 0.02 0.04 0.08 0.16 0.32 0.64)
-#bets=(0.01 0.02 0.03 0.05 0.08 0.13 0.21 0.34 0.55)
-accuracy=58
+function print_help {
+  echo "Skill vs. Luck with Geometric Progression: A Monte Carlo simulation of the balance required in life (and a casino) for successful geometric progression.
+Required Parameters:
+\tStarting Amount: The inherited amount to start with.
+\tBets: The successive weights assigned to each decision until a successful one, constituting a weighted, geometric progression.
+\tAccuracy: The probability of a suscessful decision.
+Example:
+./skill_vs_luck_with_geometric_progression.sh -s 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\""
+}
+
+while getopts "hs:b:a:" o; do
+  case $o in
+    s) startingAmount=$OPTARG ;;
+    b) bets=($OPTARG) ;;
+    a) accuracy=$OPTARG ;;
+    *) print_help ;;
+  esac
+done
+[ -z $startingAmount ] && print_help && exit
+
 invAccuracy=$((100-accuracy))
-
 amount=$startingAmount
 currBetIndex=0
 count=0
+echo "Starting Amount: $startingAmount."
+echo "Bets: ${bets[@]}."
+echo "Accuracy: $accuracy."
 echo "Started betting..."
 while [ $(echo "$amount>1"|bc) -eq 1 ]; do
   printf "Iteration %d: " "$count"
