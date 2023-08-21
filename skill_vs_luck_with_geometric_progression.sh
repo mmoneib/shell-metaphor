437e03b8f72a509fb230b4533cf395455e0a8464
Muhammad Moneib <mmoneib@gmx.com>
2023-08-22 01:53:51 +0200

    Added exitAmount parameter to skill_vs_luck_with_geometric_progression.

--- skill_vs_luck_with_geometric_progression.sh
+++ skill_vs_luck_with_geometric_progression.sh
@@ -11,18 +11,21 @@
 function print_help {
   echo "Skill vs. Luck with Geometric Progression: A Monte Carlo simulation of the balance required in life (and a casino) for successful geometric progression.
 Required Parameters:
-\tStarting Amount: The inherited amount to start with.
-\tBets: The successive weights assigned to each decision until a successful one, constituting a weighted, geometric progression.
-\tAccuracy: The probability of a suscessful decision.
+\tStarting Amount (s): The inherited amount to start with.
+\tBets (b): The successive weights assigned to each decision until a successful one, constituting a weighted, geometric progression.
+\tAccuracy (a): The probability of a suscessful decision.
+Optional Parmeters:
+\tExit Amount (x): The amount at which the simulation ends.
 Example:
 ./skill_vs_luck_with_geometric_progression.sh -s 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\""
 }
 
-while getopts "hs:b:a:" o; do
+while getopts "hs:b:a:x:" o; do
   case $o in
     s) startingAmount=$OPTARG ;;
     b) bets=($OPTARG) ;;
     a) accuracy=$OPTARG ;;
+    x) exitAmount=$OPTARG ;;
     *) print_help ;;
   esac
 done
@@ -37,14 +40,12 @@ echo "Bets: ${bets[@]}."
 echo "Accuracy: $accuracy."
 echo "Started betting..."
 while [ $(echo "$amount>1"|bc) -eq 1 ]; do
-  printf "Iteration %d: " "$count"
   choice=$((RANDOM%100))
-  printf "Choice=%d, " "$choice"
   perc=${bets[currBetIndex]}
-  printf "Delta=%f, " "$perc"
   [ $choice -ge $invAccuracy ] && amount=$(echo "scale=6;$amount+$amount*$perc" | bc -l) && currBetIndex=0 
   [ $choice -lt $invAccuracy ] && amount=$(echo "scale=6;$amount-$amount*$perc" | bc -l) && currBetIndex=$(((currBetIndex+1)%${#bets[@]}))
-  printf "Amount=%f.\n" "$amount"
+  printf "Iteration %d: Choice=%d, Delta=%f, Amount=%f.\n" "$count" "$choice" "$perc" "$amount"
+  [ ! -z $exitAmount ] && [ $(echo "$exitAmount<=$amount"| bc) -eq 1 ] && exit
   count=$((count+1))
 done
 echo "Ended with bankruptcy."
