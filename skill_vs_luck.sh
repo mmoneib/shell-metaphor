73822fc1165c7c6cec8439838a1b008d5708cd89
Muhammad Moneib <mmoneib@gmx.com>
2024-03-29 16:20:53 +0100

    Combined geometric and arithmetic progression into skill_vs_luck.

--- skill_vs_luck.sh
+++ skill_vs_luck.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 ################################################################################
-# Skill vs. Luck with Geometric Progression                                    #
+# Skill vs. Luck                                                               #
 #                                                                              #
 # A Monte Carlo simulation of the balance required in life (and a casino) for  #
-# successful geometric progression.                                            #
+# successful arithmetic or geometric progression.                              #
 #                                                                              #
 # Conceptualized and developed by: Muhammad Moneib                             #
 ################################################################################
 
 function print_help {
-  echo -e "Skill vs. Luck with Geometric Progression: A Monte Carlo simulation of the balance required in life (and a casino) for successful geometric progression.
+  echo -e "Skill vs. Luck: A Monte Carlo simulation of the balance required in life (and a casino) for successful arithmetic or geometric progression.
 Required Parameters:
-\tStart Amount (s): The inherited amount to start with.
+\tInitial Amount (i): The inherited amount to start with.
 \tBets (b): If assignation is sequential, it is the successive weights assigned to each decision until a successful one, constituting a weighted, geometric progression. If not, the weights are assigned at random.  All bets should positive in case of AppliedSign set to 'random'.
 \tAssignation (g): The assignation of bets, either 'sequential' or 'random'.
 \tAppliedSign (p): If 'fixed', the polarity of the geometric progression is dependant on the sign of the bet. If 'random', the probability of a positive progression will depend on the accuracy.
+\tStrategy (s): If 'arithmetic', the bet value will be added to or subtracted from the staked amount (arithmetic progression). If 'geometric', the bet value multiplied by the staked amount will be added to or subtracted from that staked amount (geometric progression).
 Optional Parmeters:
 \tAccuracy (a): The probability of a suscessful decision. Only applicable for 'random' AppliedSign.
 \tExit Amount (x): The amount at which the simulation ends.
 \tIs Raw Output? (r): If present, the ouput will be CSV.
 Examples:
 # Explicit skill quantification through a threshold of succes with luck modelled by unbiased randomness and progress through sequential scaling.
-$0 -s 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\" -g sequential -p random -x 1000000
+$0 -i 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\" -g sequential -p random -x 1000000
 # Implicit skill representation through enumeration of choices of geometric coefficient with luck qualified by a random choice among them.
-$0 -s 100 -b \"0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 -0.4\" -g random -p fixed -x 100000
+$0 -i 100 -b \"0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 -0.4\" -g random -p fixed -x 100000
 # Chaotic intertwining of skill and luck through explicit, quantified skill threshold and progressing through a random choice among geometric coefficients.
-$0 -s 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\" -g random -p random -x 1000000"
+$0 -i 100 -a 55 -b \"0.1 0.3 0.5 0.7 0.9\" -g random -p random -x 1000000"
   exit
 }
 
@@ -34,29 +35,31 @@ function print_error {
   exit 1
 }
 
-while getopts "hs:b:a:x:g:p:r" o; do
+while getopts "hi:b:a:x:g:p:rs:" o; do
   case $o in
-    s) startAmount=$OPTARG ;;
+    i) initialAmount=$OPTARG ;;
     b) bets=($OPTARG) ;;
     a) accuracy=$OPTARG ;;
     x) exitAmount=$OPTARG ;;
     g) assignation=$OPTARG ;;
     p) appliedSign=$OPTARG ;;
+    s) strategy=$OPTARG ;;
     r) isRawOutput=1 ;;
     *) print_help ;;
   esac
 done
-[ -z $startAmount ] && print_help && exit
+[ -z $initialAmount ] && print_help && exit
 [ "$assignation" == "sequential" ] || [ "$assignation" == "random" ] || print_error "The parameter 'assignation' is required and set to either 'sequential' or 'random'."
 [ "$appliedSign" == "fixed" ] || [ "$appliedSign" == "random" ] || print_error "The parameter 'appliedSign' is required and set to either 'fixed' or 'random'."
-[ "$appliedSign" == "random" ] && [ -z "$accuracy" ] && print_error "The parameter 'accuracy' is required when 'aapliedSign' is set to 'random'."
+[ "$appliedSign" == "random" ] && [ -z "$accuracy" ] && print_error "The parameter 'accuracy' is required when 'appliedSign' is set to 'random'."
 [ "$appliedSign" == "fixed" ] && [ -z "$assignation" == "sequential" ] && print_error "The combination of the parameters 'assignation' set 'sequential' and 'appliedSign' set to 'fixed' is not allowed."
+[ ! "$strategy" == "arithmetic" ] && [ ! "$strategy" == "geometric" ] && print_error "The parameter 'strategy' must be equal to either 'arithmetic' or 'geometric'."
 
 invAccuracy=$((100-accuracy))
-amount=$startAmount
+amount=$initialAmount
 currBetIndex=0
 count=0
-echo "Starting Amount: $startAmount."
+echo "Starting Amount: $initialAmount."
 echo "Bets: ${bets[@]}."
 echo "Accuracy: $accuracy."
 echo "Assignation: $assignation."
@@ -70,7 +73,11 @@ while [ $(echo "$amount>1"|bc) -eq 1 ]; do
   [ "$appliedSign" == "random" ] && choice=$((RANDOM%100))
   [ "$assignation" == "sequential" ] && [ $choice -ge $invAccuracy ] && currBetIndex=0 
   [ "$assignation" == "sequential" ] && [ $choice -lt $invAccuracy ] && sign=-1 && currBetIndex=$(((currBetIndex+1)%${#bets[@]}))
-  amount=$(echo "scale=6;$amount+$amount*$perc*$sign" | bc -l) 
+  if [ "$strategy" == "geometric" ]; then 
+    amount=$(echo "scale=6;$amount+$amount*$perc*$sign" | bc -l)
+  elif [ "$strategy" == "arithmetic" ]; then
+    amount=$(echo "scale=6;$amount+$perc*$sign" | bc -l)
+  fi
   [ -z $isRawOutput ] && template="Iteration %d: Choice=%d, Delta=%f, Amount=%f.\n" || template="%d,%d,%f,%f\n"
    printf "$template" "$count" "$choice" "$(echo "0+$perc*$sign"|bc -l)" "$amount" 
   [ ! -z $exitAmount ] && [ $(echo "$exitAmount<=$amount"| bc) -eq 1 ] && exit
