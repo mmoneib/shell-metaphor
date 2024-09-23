4aa4c959c3159a56e55969ecd146f755b783e00e
Muhammad Moneib <mmoneib@gmx.com>
2024-09-23 23:57:34 +0200

    Added TODO and description to matching_maximizer_satisficer.

--- matching_maxmizer_satisficer.sh
+++ matching_maxmizer_satisficer.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+################################################################################
+# Matching Saticficers with Maximizers                                         #
+#                                                                              #
+# A simple Monte Carlo simulation to analyze scarcity when matchcmaking a      #
+# groups of satisficers with maximizers. The simulation can provide inside in  #
+# situations when there is a descrepancy between the needs of two groups and   #
+# such a group behaviour can affect individuals. It can be viewed as a         #
+# simplification of the job market, dating, and trading among unqual partners  #
+# aming other real-life situations.                                            @
+#                                                                              #
+# Conceptualized and developed by: Muhammad Moneib                             #
+################################################################################
+
+# TODO Add help
+# TODO Make variables configurable through input parameters.
+# TODO Add subjectively_random strategyOfScoring where the object gets a random score with each match.
+# TODO Add prettyb and raw output.
+# TODO Add random_with_elementation strategyOfMatching to remove mathced elements from the pool.
+# TODO Add expiry of match to simulate end of relation.
+# TODO Experiment with other distributions of scores.
 
 numOfMatches=1000
 numOfMaximizers=100
