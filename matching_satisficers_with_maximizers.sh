b5b8edd78b91b0522b66da385396ff078ec46aaf
Muhammad Moneib <mmoneib@gmx.com>
2024-09-24 00:02:34 +0200

    Rename matching_maxmizer_satisficer to matching_satisficers_with_maximizers.

--- matching_satisficers_with_maximizers.sh
+++ matching_satisficers_with_maximizers.sh
@@ -27,27 +27,27 @@ maximizerThreshold=95
 satisficerThreshold=60
 strategyOfMatching="random"
 strategyOfScoring="objectively_random"
-maxmizers=()
+maximizers=()
 satisficers=()
 o_isMatch=0
 o_output=""
 
 if [ "$strategyOfScoring" == "objectively_random" ]; then
   for (( i=0;i<$numOfMaximizers;i++ )); do
-    maxmizers[$i]=$(( RANDOM%101 ))
+    maximizers[$i]=$(( RANDOM%101 ))
   done
   for (( i=0;i<$numOfSatisficers;i++ )); do
     satisficers[$i]=$(( RANDOM%101 ))
   done
 fi
-#echo "${maxmizers[@]}"
+#echo "${maximizers[@]}"
 
 for (( i=0;i<$numOfMatches;i++ )); do
   o_isMatch=0
   if [ "$strategyOfMatching" == "random" ]; then
     maximizer=$(( RANDOM%numOfMaximizers ))
     satisficer=$(( RANDOM%numOfSatisficers ))
-    maximizerScore=${maxmizers[$maximizer]}
+    maximizerScore=${maximizers[$maximizer]}
     satisficerScore=${satisficers[$satisficer]}
     if [ $maximizerScore -ge $satisficerThreshold ] && [ $satisficerScore -ge $maximizerThreshold ]; then
       o_isMatch=1
