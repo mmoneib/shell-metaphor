3a741404860604cdaaa6cb98892f54eeb6482725
Muhammad Moneib <mmoneib@gmx.com>
2025-03-16 01:51:26 +0100

    Modified differentiation between the mechanisms of satificers and maximozers to depend on a set of probabilities.

--- satisficer_vs_maximizer.sh
+++ satisficer_vs_maximizer.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#TODO Move to functiona and add options other than this independent time series (add dependent, [percentages...etc.).
+#TODO Validate that maximizer's values are more than sitisficers.
+
+probabilities="-3 -2 -1 1 2 3" # -ve for stones, +ve for carrots."
 
 satisficerScore=0
 maximizerScore=0
@@ -8,20 +12,18 @@ satisficerCache=0
 maximizerCache=0
 SatisficerScore=0
 MaximizerScore=0
+ifs= read -a probabilitiesArr <<< "$probabilities"
+numOfProbabilities=${#probabilitiesArr[@]}
 
 echo "Is it better to be a satisficer in life or a maximizer?"
 echo "Let's see through a Monte Carlo simiulation in which life throws either a carrot or a stone in each iteration."
-echo "Satificer will accumulate carrots in 100s."
-echo "Maximizer will accumulate carrots in 1000s."
-echo "Who will get the more carrots?"
+echo "Who will get the more carrots? And who will avoid more stones?"
 count=0
 while true; do
   [ $((count%1)) -eq 0 ] && echo "After $count iterations, Satisficer has $satisficerScore carrots, while Maximizer has $maximizerScore carrots. Difference = $((satisficerScore-maximizerScore))"
-  delta=$(($RANDOM%2+1)) # -1 or 1
-  if [ $delta -eq 1 ]; then #TODO Move to functiona and add options other than this independent time series (add dependent, [percentages...etc.).
-    satisficerCache=$((satisficerCache+1))
-    maximizerCache=$((maximizerCache+1))
-  fi
+  delta=i${probabilities[$(( RANDOM%numOfProbabilities ))}]
+  satisficerCache=$((satisficerCache+1))
+  maximizerCache=$((maximizerCache+1))
   [ $satisficerCache -eq $satisficerAmbition ] && satisficerScore=$((satisficerScore+satisficerCache)) && satisficerCache=0
   [ $maximizerCache -eq $maximizerAmbition ] && maximizerScore=$((maximizerScore+maximizerCache)) && maximizerCache=0
   count=$((count+1))
