98e1f13bf3090d1ef7bf194b1832c85f6cb95d0b
Muhammad Moneib <mmoneib@gmx.com>
2025-03-18 00:19:31 +0100

    Added random_walk strategy to coincidence with refactoring and enhanced design.

--- coincidence.sh
+++ coincidence.sh
@@ -7,57 +7,87 @@
 #                                                                                           #
 # Conceptualized and developed by: Muhammad Moneib                                          #
 #############################################################################################
-# TODO Add option to manage pauses between frames.
 # TODO Develop the other 2 strategies.
-# TODO Add comments including rationale behind design decisions.
+# TODO Check why problematic on Mac starting from l 4 and w 4. Takes much longer than expected.
 
-while getopts "l:w:" opt; do
+function print_error {
+  echo "ERROR: $1"
+  print_usage
+}
+
+function print_usage {
+  echo "USAGE: $0 -s strategy_of_agent1_here -S strategy_of_agent2_here -p pause_interval_here [-l height_here -w width_here]"
+  exit
+}
+
+# Constants
+strategies=( "random_walk" "stationary" "teleportation" ) # For validation.
+# Arguments parsing
+while getopts "l:p:s:S:w:h" opt; do
   case "$opt" in
   l) height=$OPTARG ;;
+  p) pauseInterval=$OPTARG ;;
+  s) agent1Strategy=$OPTARG ;;
+  S) agent2Strategy=$OPTARG ;;
   w) width=$OPTARG ;;
+  h) print_usage
   esac
 done
-
+# Arguments validation
+[ -z "$agent1Strategy" ] && print_error "Missing strategy_of_agent1."
+[ -z "$agent2Strategy" ] && print_error "Missing strategy_of_agent2."
+matchCount=0
+for (( i=0; i<${#strategies[@]}; i++ )); do
+  [ "$agent1Strategy" == "${strategies[i]}" ] && (( matchCount++ ))
+  [ "$agent2Strategy" == "${strategies[i]}" ] && (( matchCount++ ))
+done
+[ $matchCount -ne 2 ] && print_error "A supplied strategy must be one of the values 'random_walk', 'teleportation', or 'stationary'!"
 [ -z "$width" ] && width=$(tput cols) || customSize=true
 [ -z "$height" ] && height=$(tput lines) || customSize=true
-
+# Initialization commands
+tput reset
+# Initialization of internal variables
+deltas=( -1 1 +$width -$width )
 #resize -s $height $width
 spaceSize=$(( width*height ))
-space=()
-agent1strategy="teleporation" # "random-walk" "stationary"
-agent2strategy="teleporation" # "random-walk" "stationary"
-
-agent1Position=0
-agent2position=0
-
+space=() # The canvas as an array makes it easier to print, manipulate, and add while printing.
+agent1Position="$(( RANDOM%spaceSize ))"
+agent2Position="$(( RANDOM%spaceSize ))"
 count=0
-for (( i=0; i<$spaceSize; i++ )); do
-  if [ "$customSize" == "true" ] && [ $(( i%width )) -eq 0  ]; then
-    o_space+=("\n")
-  fi 
+for (( i=0; i<$spaceSize; i++ )); do # Initializing Space
   space+=(".")
 done
-tput reset
+# Processing
 while true; do
-  tput cup 0 0
   space[$agent1Position]="."
   space[$agent2Position]="."
   function teleportation {
     echo "$(( RANDOM%spaceSize ))"
   }
-  agent1Position="$(teleportation)"
-  agent2Position="$(teleportation)"
+  function random_walk {
+    oldPosition=$1
+    delta=${deltas[ RANDOM%${#deltas[@]} ]}
+    [ $(( oldPosition%width )) -eq $(( width-1 )) ] && [ $delta -eq 1 ] && delta=0 # Right edge as a barrier.
+    [ $(( oldPosition%width )) -eq 0 ] && [ $delta -eq -1 ] && delta=0 # Left edge as a barrier.
+    newAgentPosition=$(( $oldPosition+$delta )) 
+    [ $newAgentPosition -ge 0 ] && [ $newAgentPosition -lt $spaceSize ] &&  echo "$newAgentPosition" || echo $oldPosition
+  }
+  agent1Position=$($agent1Strategy $agent1Position)
+  agent2Position=$($agent2Strategy $agent2Position)
+  tput cup 0 0 # Faster than reset or clear.
   space[$agent1Position]="1"
   space[$agent2Position]="2"
-  if [ "$customSize" == "$true" ]; then
-    printf "%s" "${space[@]}"
+  # Output
+  if [ "$customSize" != "true" ]; then
+    printf "%s" "${space[@]}" # Better flowing performance, as new line are free through text wrapping.
   else
     for (( i=0; i<$height; i++ )); do
-      echo "${space[@]:$(( i*width  )):width}"
+      echo "${space[@]:$(( i*width  )):width}" # Printing each line individually to do new lines manually for a smaller canvas within the windwo. It is more convenient for the user than using the resize command.
     done
   fi
   (( count++ ))
   [ $agent1Position -eq $agent2Position ] && break
-  sleep 0.5
+  [ ! -z "$pauseInterval" ] && sleep $pauseInterval
 done
+# Report
 echo "Coincidence happened after $count movements!" 
