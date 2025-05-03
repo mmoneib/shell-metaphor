d1bcd58468e2d32d2cd1fc5e16ac8e51de8d92ae
Muhammad Moneib <mmoneib@gmx.com>
2025-05-04 01:25:59 +0200

    Added processing of risk_portfolio simulation.

--- risk_portfolio.sh
+++ risk_portfolio.sh
@@ -9,25 +9,26 @@
 #                                                                                           #
 # Conceptualized and developed by: Muhammad Moneib                                          #
 #############################################################################################
-#TODO Add initial amount.
-#TODO Add processing.
 #TODO Make risks more fine tuned to include assymetrical win/loss potentials in addition to odds.
 
 function print_usage {
   echo "USAGE: $0 -a allocations_here -c categories_here -r risks_here"
-  echo "Example: $0 -a '10,20,30,40' -c 'A,B,C,D' -r '90,70,50,10'"
+  echo "Example: $0 -i 10000 -n 50 -a '10,20,30,40' -c 'A,B,C,D' -r '90,70,50,10'"
   exit
 }
 
 categories=""
 risks=""
 allocPerc=""
+numOfIterations=100
 # Arguments parsing.
-while getopts "a:c:r:h" opt; do
+while getopts "a:c:i:n:r:h" opt; do
   case $opt in
     a) allocPerc="$OPTARG" ;;
     c) categories="$OPTARG" ;;
+    i) initialAmount="$OPTARG" ;;
     r) risks="$OPTARG" ;;
+    n) numOfIterations="$OPTARG" ;;
     h) print_usage ;;
     *) print_usage ;;
   esac
@@ -42,11 +43,33 @@ IFS=,; read -a catsArr <<< "$categories"
 IFS=,; read -a risksArr <<< "$risks"
 IFS=,; read -a allocsArr <<< "$allocPerc"
 # Validation of internal variables.
-[ ${#catsArr[@]} -ne ${#riskArr[@]} ] || [ ${riskArr[@]} -ne ${allocsArr[@]} ] && echo "ERROR: Number of categories, their associated risks, and their allocated percentages must be the same."
+[ ${#catsArr[@]} -ne ${#risksArr[@]} ] || [ ${#risksArr[@]} -ne ${#allocsArr[@]} ] && echo "ERROR: Number of categories, their associated risks, and their allocated percentages must be the same." && exit 1
 allocsSum=0
 for (( i=0; i<${#allocsArr[@]}; i++ )); do
   allocsSum=$(( allocsSum+${allocsArr[$i]}  ))
 done
 [ $allocsSum -ne 100 ] && echo "ERROR: The sum of allocations must be equal to 100."
 # Processing
-
+echo "Initial Amount = $initialAmount, to be allocated over ${#catsArr[@]} risk categories."
+allocatedAmounts=()
+allocsSize=${#allocsArr[@]}
+for (( i=0; i<$allocsSize; i++ )); do
+  allocatedAmounts[i]=$(echo "scale=2;$initialAmount*${allocsArr[$i]}/100"|bc -l)
+done
+allocationsStr=""
+for (( i=0; i<${#allocatedAmounts[@]}; i++ )); do
+  allocationsStr+="${catsArr[$i]}=${allocatedAmounts[$i]} -- "
+done
+echo "Iteration 0: $allocationsStr"
+count=0
+while [ $(( ++count )) -lt $numOfIterations ]; do
+  allocationsStr=""
+  for (( i=0; i<${#allocatedAmounts[@]}; i++ )); do
+    amountOfCategory=${allocatedAmounts[i]}
+    riskOfCategory=${risksArr[i]}
+    [ $(( RANDOM%100 )) -ge $riskOfCategory ] && deltaFactor=1 || deltaFactor=-1
+    allocatedAmounts[i]=$(echo "scale=2;$amountOfCategory+($deltaFactor*$amountOfCategory*$riskOfCategory)/100"|bc -l)
+  allocationsStr+="${catsArr[$i]}=${allocatedAmounts[$i]} -- "
+  done
+  echo "Iteration $count: $allocationsStr"
+done
