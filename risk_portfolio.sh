3c5db444908988c543b12233fba53c138f3f5366
Muhammad Moneib <mmoneib@gmx.com>
2025-05-07 23:26:22 +0200

    Added total count to risk_portfolio, polished its output along with other enhancements.

--- risk_portfolio.sh
+++ risk_portfolio.sh
@@ -10,6 +10,8 @@
 # Conceptualized and developed by: Muhammad Moneib                                          #
 #############################################################################################
 #TODO Make risks more fine tuned to include assymetrical win/loss potentials in addition to odds.
+#TODO Add template for echoing.
+#TODO Add optional percentage of amount gained per iteration.
 
 function print_usage {
   echo "USAGE: $0 -a allocations_here -c categories_here -r risks_here"
@@ -48,28 +50,36 @@ allocsSum=0
 for (( i=0; i<${#allocsArr[@]}; i++ )); do
   allocsSum=$(( allocsSum+${allocsArr[$i]}  ))
 done
-[ $allocsSum -ne 100 ] && echo "ERROR: The sum of allocations must be equal to 100."
+[ $allocsSum -ne 100 ] && echo "ERROR: The sum of allocated percentages must be equal to 100." && exit 1
 # Processing
 echo "Initial Amount = $initialAmount, to be allocated over ${#catsArr[@]} risk categories."
 allocatedAmounts=()
 allocsSize=${#allocsArr[@]}
-for (( i=0; i<$allocsSize; i++ )); do
-  allocatedAmounts[i]=$(echo "scale=2;$initialAmount*${allocsArr[$i]}/100"|bc -l)
+for (( i=0; i<$allocsSize; i++ )); do # Preparation of initial amounts per category.
+  allocatedAmounts[i]=$(printf %.2f $(echo "scale=2;$initialAmount*${allocsArr[$i]}/100"|bc -l))
 done
 allocationsStr=""
+totalAllocations=0
 for (( i=0; i<${#allocatedAmounts[@]}; i++ )); do
   allocationsStr+="${catsArr[$i]}=${allocatedAmounts[$i]} -- "
 done
+totalAllocations=$initialAmount
+allocationsStr+="Total=$totalAllocations"
 echo "Iteration 0: $allocationsStr"
 count=0
 while [ $(( ++count )) -lt $numOfIterations ]; do
   allocationsStr=""
+  totalAllocations=0
+  totalAllocationsEquation="0" # Used to prepare the statement to be calculated by 'bc'; reduces expensive calls.
   for (( i=0; i<${#allocatedAmounts[@]}; i++ )); do
     amountOfCategory=${allocatedAmounts[i]}
     riskOfCategory=${risksArr[i]}
     [ $(( RANDOM%100 )) -ge $riskOfCategory ] && deltaFactor=1 || deltaFactor=-1
-    allocatedAmounts[i]=$(echo "scale=2;$amountOfCategory+($deltaFactor*$amountOfCategory*$riskOfCategory)/100"|bc -l)
-  allocationsStr+="${catsArr[$i]}=${allocatedAmounts[$i]} -- "
+    allocatedAmounts[i]=$(printf %.2f $(echo "scale=2;$amountOfCategory+($deltaFactor*$amountOfCategory*$riskOfCategory)/100"|bc -l))
+    allocationsStr+="${catsArr[$i]}=${allocatedAmounts[$i]} -- "
+    totalAllocationsEquation+="+${allocatedAmounts[$i]}"
   done
-  echo "Iteration $count: $allocationsStr"
+  totalAllocations=$(printf %.2f "$(echo "scale=2;$totalAllocationsEquation"|bc -l)" )
+  allocationsStr+="total=$totalAllocations"
+  echo "Iteration $count: $allocationsStr"|sed 's/ -- $//g'
 done
