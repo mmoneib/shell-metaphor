89df9099d16d186b96cf87f6324b76c0c2b1d83d
Muhammad Moneib <mmoneib@gmx.com>
2026-01-12 23:33:10 +0100

    Changed loss_minimization_stochastic logic to that of curve fitting.

--- loss_minimization_stochastic.sh
+++ loss_minimization_stochastic.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 ################################################################################
-# Stochastic Equation Solver                                                   #
+# Stochastic Fitting Loss Minimization                                         #
 #                                                                              #
-# A simple stochastic simulation of iterative minimization of loss towards     #
-# getting the solution of anyaffine function . The function is in the form of  #
-# c1*a+c2*b+...+c.                                                             #
+# A simple script to fit a curve to provided data points and their             #
+# corresponding expected outputs using loss minimization technique in          #
+# combination with stochastic progression.                                     #
 #                                                                              #
 # Conceptualized and developed by: Muhammad Moneib                             #
 ################################################################################
 
-#TODO Add more strategies to variable adjustment.
-#TODO Support non-linear.
-#TODO Script for linear regression based on input table of independent (input)/dependent (output) pair of variables.
-
 # Configuration
 ## Support Functions
 function __print_usage {
-  echo "$0 -c coefficients_list_here -e expected_output_here -n num_of_runs_here"
-  echo "Example: $0 -c 2,1,4,7 -e 13 -n 1000"
+  echo "$0 -i inputs_list_here -n num_of_runs_here -o outputs_expected_list_here"
+  echo "Example: $0 -i 1,2,3,4 -o 4,7,10,13 -n 1000"
   exit
 }
 ## Fallback
@@ -25,60 +21,73 @@ function __print_usage {
 ## Defaults
 isVerbose=0
 ## Inputs
-while getopts "c:e:n:v" o; do
+while getopts "d:i:n:o:v" o; do
   case $o in
-  c) coefficientsList=$OPTARG ;;
-  e) expectedY=$OPTARG ;;
+  i) inputsList=$OPTARG ;;
   n) numOfRuns=$OPTARG ;;
+  o) outputsList=$OPTARG ;;
   v) isVerbose=1 ;;
   *) __print+usage ;;
   esac
 done
-IFS=,; read -a coefficientsArr <<< "$coefficientsList"
-numOfVariables=$(( ${#coefficientsArr[@]}-1 )) # Variables + Constant --> Coefficients.
+IFS=,; read -a inputsArr <<< "$inputsList"
+IFS=,; read -a outputsArr <<< "$outputsList"
+numOfInputs=${#inputsArr[@]} 
 ## Validation
 [ -z $numOfRuns ] && __print_usage
+[ $numOfInputs -ne ${#outputsArr[@]} ] && echo "ERROR: Number ov inputs must be the same as that of outputs." && exit 1
 
 # Processing
 ## Support Functions
-function random_variable { 
+function random_parameter { 
   sign=$(( RANDOM%2 ))
   [ $sign -eq 0 ] && sign=-1
   echo $(( RANDOM%100 * sign )) 
 }
+
 ## Defaults
-res=0
+numOfParams=1
+
 loss=9999999999999999
+params=()
 ## Engine
 for (( count=0; count<$numOfRuns; count++ )); do
   ### Initialization
-  vars=()
-  lastVars=()
-  ### Equation Build Up
-  eq=""
-  for (( i=0; i<$numOfVariables; i++ )); do
-    lastVars[i]=${vars[i]}
-    var=$(random_variable)
-    vars[i]=$var
-    eq+="${coefficientsArr[i]}*$var+"
+  errors=()
+  lastParams=()
+  for (( pi=0;pi<$numOfParams;pi++ )); do
+    lastParams[pi]=${params[pi]}
+    param=$(random_parameter)
+    params[pi]=$param 
   done
-  eq+="${coefficientsArr[$numOfVariables]}"
-  ### Equation Evaluation
-  res=$(( $(echo $eq) ))
+  interceptor=$(random_parameter)
+  ### Equation Build Up
+  for (( i=0; i<$numOfInputs; i++ )); do
+    #### Evaluate Equation
+    y=$(( ${param[0]}*${inputsArr[$i]}+$interceptor )) # Assume linear fitting. TODO: Support higher orders based on numOfParams.
+#echo " y=${param[0]}*${inputsArr[$i]}+$interceptor"
+    #### Error Calculation
+    errorToAdd=$(( $y-${outputsArr[$i]} ))
+    errors[i]=$errorToAdd
+  done
   ### Loss Calculation
   lastLoss=$loss
   [ $loss -eq 0 ] && echo "Optimum solution found. No further loss minimization is possible." && break
-  loss=$(( expectedY-res ))
-  [ $loss -lt 0 ] && loss=$((  $loss*-1 )) # abs
+  loss=0
+  for (( ei=0;ei<${#errors[@]};ei++ )); do
+    error=${errors[$ei]}
+    [ $error -lt 0 ] && error=$(( $error*-1 )) # abs.
+    loss=$(( $loss+$error ))
+  done
   if [ $loss -lt $lastLoss ]; then
     #### Loss Improvement
-    lastMinimizationStr="Iteration: $count -- Equation: $eq -- Result: $res -- Loss: $loss"
+    lastMinimizationStr="Iteration: $count -- Loss: $loss -- Parameters: ${params[@]} -- Interceptor: $interceptor"
     echo $lastMinimizationStr
   else
     #### Loss Fallback
     loss=$lastLoss
-    for (( i=0; i<$numOfVariables; i++ )); do
-      vars[i]=${lastVars[i]}
+    for (( i=0; i<$numOfParams; i++ )); do
+      params[i]=${lastParams[i]}
     done
     [ $isVerbose -eq 1 ] && echo "Iteration: $count -- Skipped..."
   fi
